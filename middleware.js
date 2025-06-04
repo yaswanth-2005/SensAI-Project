4aa4ed284d2c548f35c3e7e27bdb7e2e5facbf4c
Yaswanth Varada <yaswanthvarada20@gmail.com>
2025-06-04 12:31:15 +0530

    Middle ware setup and Clerk SetUp done

--- middleware.js
+++ middleware.js
@@ -1,6 +1,25 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
+// const isProtectedRoute = createRouteMatcher([
+//   "/dashboard(.*)",
+//   "/resume(.*)",
+//   "/interview(.*)",
+//   "/ai-cover-letter(.*)",
+//   "/onboarding(.*)",
+// ]);
+
+// export default clerkMiddleware(async (auth, req) => {
+//   const { userId } = await auth();
+
+//   if (!userId && isProtectedRoute(req)) {
+//     const { redirectToSignIn } = await auth();
+//     return redirectToSignIn();
+//   }
+
+//   return NextResponse.next();
+// });
+
 const isProtectedRoute = createRouteMatcher([
   "/dashboard(.*)",
   "/resume(.*)",
