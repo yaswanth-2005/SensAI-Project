c976e45850564f515c2772ebad43dcb534e4d345
Yaswanth Varada <yaswanthvarada20@gmail.com>
2025-06-04 17:36:34 +0530

    Inngest Client Initiated

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -1,5 +1,22 @@
-import { SignIn } from "@clerk/nextjs";
+// This type of folder strucutre is called as Catch all route
 
-export default function Page() {
-  return <SignIn />;
+//[[...sign-in]]
+// [[...sign-up]]
+
+
+//(auth) -> called as Route group by giving this this name wouldn't be considered under the routes by using this
+// we can organize the route groups.. Clearly..
+
+
+// Above two folder strucures are called as Catch-all Routes..
+
+import { SignIn } from '@clerk/nextjs'
+import React from 'react'
+
+const page = () => {
+  return (
+    <SignIn />
+  )
 }
+
+export default page
